5b1447bf9b7ea4b5d1db90f9599ec07d34e8b2dc
s4iTen <ychouk102@gmail.com>
2023-04-14 11:48:11 +0200

    this function request the api structure of the db and gives the employer a todo list

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""this is the API requestes and the handling of the data"""
+
+
+import requests
+import sys
+
+
+employee_id = sys.argv[1]
+
+# Fetch the employee data
+str = 'https://jsonplaceholder.typicode.com/users/'
+response = requests.get(f"{str}{employee_id}")
+e_data = response.json()
+
+# Fetch the TODO list for the employee
+strus = 'https://jsonplaceholder.typicode.com/todos?userId='
+response = requests.get(f"{strus}{employee_id}")
+todo_list = response.json()
+
+# Count the number of completed tasks
+completed_tasks = [task for task in todo_list if task["completed"]]
+num_completed_tasks = len(completed_tasks)
+
+# Print the TODO list progress
+d = " is done with tasks("
+print(f"Employee {e_data['name']}{d}{num_completed_tasks}/{len(todo_list)}):")
+for task in completed_tasks:
+    print(f"\t{task['title']}")
