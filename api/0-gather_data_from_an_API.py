46dc0da73d3b474ae1289abfb3bef4da0d4732a9
s4iTen <ychouk102@gmail.com>
2023-04-17 14:44:12 +0200

    this is the last updates

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """this is the API requestes and the handling of the data"""
 
-import json
+
+import requests
 import sys
-from urllib import request
+
 
 if __name__ == '__main__':
     """This is the requests of the api and the usage of it"""
     employee_id = sys.argv[1]
 
     # Fetch the employee data
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    with request.urlopen(url) as f:
-        e_data = json.loads(f.read())
+    str = 'https://jsonplaceholder.typicode.com/users/'
+    response = requests.get(f"{str}{employee_id}")
+    e_data = response.json()
 
     # Fetch the TODO list for the employee
-    url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    with request.urlopen(url) as f:
-        todo_list = json.loads(f.read())
+    strus = 'https://jsonplaceholder.typicode.com/todos?userId='
+    response = requests.get(f"{strus}{employee_id}")
+    todo_list = response.json()
 
     # Count the number of completed tasks
-    completed_tasks = [task for task in todo_list if task["completed"] == True]
+    completed_tasks = [task for task in todo_list if task["completed"] == 1]
     n_tasks = len(completed_tasks)
 
     # Print the TODO list progress
