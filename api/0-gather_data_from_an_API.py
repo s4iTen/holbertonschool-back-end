011fb29cc521290eb3de4ee142216797e3d52138
s4iTen <ychouk102@gmail.com>
2023-04-18 13:08:52 +0200

    this is the last updates of the function that import the api data and manipulate it

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,24 +8,30 @@ import sys
 
 if __name__ == '__main__':
     """This is the requests of the api and the usage of it"""
-    employee_id = sys.argv[1]
+    id = sys.argv[1]
 
     # Fetch the employee data
     str = 'https://jsonplaceholder.typicode.com/users/'
-    response = requests.get(f"{str}{employee_id}")
+    response = requests.get(f"{str}{id}")
     e_data = response.json()
 
     # Fetch the TODO list for the employee
-    strus = 'https://jsonplaceholder.typicode.com/todos?userId='
-    response = requests.get(f"{strus}{employee_id}")
+    strus = 'https://jsonplaceholder.typicode.com/todos'
+    response = requests.get(f"{strus}")
     todo_list = response.json()
 
     # Count the number of completed tasks
-    completed_tasks = [task for task in todo_list if task["completed"] == 1]
+    completed_tasks = []
+    tasks = 0
+    for i in todo_list:
+        if i['userId'] == int(id):
+            tasks += 1
+            if i['completed'] == 1:
+                completed_tasks.append(i)
     n_tasks = len(completed_tasks)
 
     # Print the TODO list progress
     d = " is done with tasks("
-    print(f"Employee {e_data['name']}{d}{n_tasks}/{len(todo_list)}):")
+    print(f"Employee {e_data['name']}{d}{n_tasks}/{tasks}):")
     for task in completed_tasks:
         print(f"\t{task['title']}")
