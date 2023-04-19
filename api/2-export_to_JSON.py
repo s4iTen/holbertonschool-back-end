50ddaf067f0a4ca554ada0f706934953484c5180
s4iTen <ychouk102@gmail.com>
2023-04-19 13:52:54 +0200

    this fuction export the data to json file with a specific format

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''this is the importation of the modules'''
+import sys
+import json
+import requests
+
+
+# Set the employee id
+id = sys.argv[1]
+
+# Fetch the employee data
+url = f"https://jsonplaceholder.typicode.com/users/{id}"
+response = requests.get(url)
+d = response.json()
+
+# Fetch the TODO list for the employee
+url = f"https://jsonplaceholder.typicode.com/todos?userId={id}"
+response = requests.get(url)
+todo_list = response.json()
+
+# Create a dictionary with the required format
+data = {str(id): []}
+u = 'username'
+c = 'completed'
+for t in todo_list:
+    data[str(id)].append({"task": t["title"], c: t[c], "username": d[u]})
+
+# Save the data in a JSON file
+with open(f"{d['id']}.json", "w") as f:
+    json.dump(data, f)
